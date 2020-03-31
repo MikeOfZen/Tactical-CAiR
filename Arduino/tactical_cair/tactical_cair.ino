565aeee54f549cfd83da3e8ef84362ae51536f0b
MikeOfZen <michael.zl.prime@gmail.com>
2020-03-31 22:40:37 +0300

    Merge branch 'master' of https://github.com/MikeOfZen/Tactical-CAiR

--- Arduino/tactical_cair/tactical_cair.ino
+++ Arduino/tactical_cair/tactical_cair.ino
@@ -23,8 +23,8 @@ General Notes:
 //exhaust_valve configuration
 #define EXHAUST_VALVE_PIN 9  //1=open valve;0=close valve
 //Servo configuration 
-#define SERVO_START 0 //degrees
-#define SERVO_END 120  //degrees
+#define SERVO_START 80 //degrees
+#define SERVO_END 140  //degrees
 #define SERVO_SPEED 3 //1=fastest 10=slower, 20=slower etc... the actual speed depends on the servo
 //Polarity of relay settings
 #define EV_OPEN 1
@@ -348,6 +348,3 @@ void loop()
 
     
 }
-
-
-
